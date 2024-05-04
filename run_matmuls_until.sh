f62176164c1d66c871f2e22883bd728a8f0c7aeb
UmerHA <40663591+UmerHA@users.noreply.github.com>
2024-05-04 15:04:43 +0200

    Update run_matmuls_until.sh

--- run_matmuls_until.sh
+++ run_matmuls_until.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 k=$1
-echo "🫡 Going to run first $i matmul kernels"
+echo "🫡 Going to run first $k matmul kernels"
 
 # Use a C-style for loop to iterate from 1 to k
 for ((i=1; i<=k; i++)); do
-    echo "🏃🏽‍♂️ Running kernel $i"
+    echo "🏃 Running kernel $i"
     build/matmul $i
 done
